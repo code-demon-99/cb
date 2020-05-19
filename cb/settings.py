79a7dccb3e3947fd30d81a97b9148598fa6a6218
Rishi <namdev373@gmail.com>
2020-05-19 17:46:00 +0530

    first part first table

--- cb/settings.py
+++ cb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'cb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'Staticfiles ')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
+# activate heroku 
+django_heroku.settings(locals())
--- model/views.py
+++ model/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import pandas as pd
+from django.views.generic import TemplateView
+
+
+def home_view(request):
+    return render (request,'home.html')
 
 
